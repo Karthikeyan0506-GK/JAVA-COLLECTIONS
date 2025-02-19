1f542bc7c7b50a9765498af60b515012031b8ef7
Karthikeyan0506-GK <karthikeyan2k0506@gmail.com>
2025-02-19 13:47:06 +0530

    Add files via upload

---
+++ HashSetExample.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> Animals=new HashSet<>();
+
+Animals.add("Lion");
+Animals.add("Tiger");
+Animals.add("Chettah");
+Animals.add("Elephant");
+Animals.add("Tiger");
+
+System.out.println("HashSet:"+ Animals);
+
+
+
+
+
+
+
+    }
+}
